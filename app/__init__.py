e713ffcc5e50d221e98847af77e3f8433464f554
DeepeshP04 <pratapdeepesh4@gmail.com>
2025-06-20 00:11:08 +0530

    Configure mongodb_url in app

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask
-from api.users import user_bp
+from flask_pymongo import PyMongo
+from .api.users import user_bp
+from dotenv import load_dotenv
+import os
+
+load_dotenv(dotenv_path="../.env")
 
 def create_app():
-    app = Flask(__name__)
+    DB_URL = os.getenv("MONGODB_URL")
     
+    # Create flask app
+    app = Flask(__name__)
+    app.config["Mongo_URI"] = f"mongo://localhost:27017/{DB_URL}"
+    mongo = PyMongo(app)
+    
+    # Register blueprints
     app.register_blueprint(user_bp)
     
     return app

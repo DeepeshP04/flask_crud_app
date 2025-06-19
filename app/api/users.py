c40fa3cf4b10c2bd8ccd540f346fb051b39cbc28
DeepeshP04 <pratapdeepesh4@gmail.com>
2025-06-20 01:30:18 +0530

    Implement update_user_data route

--- app/api/users.py
+++ app/api/users.py
@@ -5,20 +5,23 @@ from bson import ObjectId
 
 user_bp = Blueprint("user_bp", __name__, url_prefix="/users")
 
+# Get all users
 @user_bp.route("/")
 def get_users():
     users = mongo.db.users.find()
     return jsonify({"users": users}), 200
 
-@user_bp.route("/<user_id>")
-def get_user_by_id(user_id):
-    user = mongo.db.users.find_one({"_id": ObjectId(user_id)})
+# Get a specific user by id
+@user_bp.route("/<id>")
+def get_user_by_id(id):
+    user = mongo.db.users.find_one({"_id": ObjectId(id)})
     if user:
         user["_id"] = str(user["_id"])
         return jsonify({"user": user}), 200
     else:
         return jsonify({"error": "User not found."}), 404
     
+# Create a new user
 @user_bp.route("/", methods=["POST"])
 def create_user():
     data = request.get_json()
@@ -33,3 +36,21 @@ def create_user():
     user.save()
     
     return jsonify({"message": "User created successfully.", "user": user}), 201
+
+# Update the user data
+@user_bp.route("/id")
+def update_user_data(id):
+    data = request.get_json()
+    name = data.get("name")
+    email = data.get("email")
+    password = data.get("password")
+    
+    result = mongo.db.update_one(
+        {"_id": ObjectId(id)},
+        {"$set": {"name": name, "email": email, "password": password}}
+    )
+    
+    if result.matched_count() > 0:
+        return jsonify({"message": "User details updated."}), 200
+    else: 
+        return jsonify({"error": "User not found."}), 404
